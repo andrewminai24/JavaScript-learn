e9ce4cd1e7346dd9e0d44d767526d9978c66fdb9
Andrew Minai <andrewminai24@gmail.com>
2019-06-19 05:02:41 -0700

    ES5

---
+++ ES5/classes.js
@@ -0,0 +1,16 @@
+//Lecture Classes
+//ES5
+var Person5 = function(name,yearOfBirth,job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job;
+}
+
+Person5.prototype.calculateAge = 
+function(){
+    var age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+}
+var john5 = new Person5('John',1990,'teacher');
+
+console.log(john5);
