042404bab991a56c4dfdcb5eb743727aa659e134
Andrew Minai <andrewminai24@gmail.com>
2019-06-24 13:01:08 -0700

    closure practice

---
+++ ES5/closure2.js
@@ -0,0 +1,12 @@
+//What is the output of this?
+var num = 4;
+function outer(){
+    var num = 2;
+    function inner(){
+        num++;
+        var num = 3;
+        console.log(num)
+    }
+    inner();
+}
+outer();
