9a628a400d5f5b05837c8cf09c66a4bc18964ba2
Andrew Minai <andrewminai24@gmail.com>
2019-06-24 12:58:39 -0700

    Dice project

---
+++ Dice project/events.js
@@ -0,0 +1,12 @@
+/*Events are notifications that are sent to notify the code that
+something happened to the webpage
+Clicking a button resizing a window scrolling down 
+pressing a key
+
+Event listener A function that performs an action based on a certain event is waits for that event to happen
+
+How to set up an event handler
+what a callback function is 
+what an anoymous function is 
+another way to select elements by ID
+how to change the image in an <im> element
